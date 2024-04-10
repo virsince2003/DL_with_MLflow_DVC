91f905f404026704b3d694434e0ca48dafb0c2a6
Virender Chauhan <virchauhan657@gmail.com>
2024-04-10 19:07:59 +0530

    Folder Structure

---
+++ template.py
@@ -0,0 +1,46 @@
+import os 
+import logging
+from pathlib import Path
+
+
+logging.basicConfig(level = logging.INFO, format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+project_name = "Automaitc_number_plate_recognition"
+
+files_list = [
+        ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html",
+    "main.py",
+    "app.py"
+]
+
+
+for filepath in files_list:
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
+    
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Created directory: {filedir} for file: {filename}")
+        
+    if not os.path.exists(filepath) or os.path.isfile(filepath) == 0 :
+        with open(filepath, "w") as file:
+            logging.info(f"Created file: {filename}")
+    else:
+        logging.info(f"File: {filename} already exists")
+    
+    
