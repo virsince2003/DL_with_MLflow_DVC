46a35e69cbe2dde9b1a63654f8081045fb970be0
Virender Chauhan <virchauhan657@gmail.com>
2024-04-11 12:00:03 +0530

    loggings

---
+++ src/Automaitc_number_plate_recognition/__init__.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import logging
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d')}.log"
+log_path = os.path.join(os.getcwd(), 
+                        "logs")
+
+os.makedirs(log_path,exist_ok=True)
+
+log_file_path = os.path.join(log_path, LOG_FILE)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(lineno)d - %(name)s - %(levelname)s - %(message)s]",
+    handlers=[
+        logging.FileHandler(log_file_path),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger("Automatic_number_plate_recognition")
+
+
+
+
